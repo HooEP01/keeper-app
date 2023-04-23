3864b06922cede3fe2940f19dfb34c31d372bcc2
HOOEP01 <B200152B@sc.edu.my>
2023-04-23 23:27:23 +0800

    props and map

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../note";
+
+
 
 function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello App</h1>
+      {notes.map((note) => 
+      <Note key={note.key} title={note.title} content={note.content}/>
+      )}
+      <Footer/>
     </div>
   );
 }
